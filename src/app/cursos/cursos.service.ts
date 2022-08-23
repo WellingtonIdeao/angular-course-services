e2be56560dff1032f6ed52dbffea9f5703277cfe
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-23 19:03:48 -0300

    Escopo de instancias de servicos e modulos

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class CursosService {
 
+    private cursos: string [] = ['Angular 2', 'Python', 'Django'];
+
+    constructor(){
+        console.log('CursosService');
+    }
+
     getCursos(): string[]{
-        return ['Angular 2', 'Python', 'Django'];
+        return this.cursos;
+    }
+
+    addCurso(curso: string): void{
+        this.cursos.push(curso);
     }
 }
